b013a426e268f22ee3729807b911c48f6b825dcf
Manoj <manojtambi2215889@gmail.com>
2026-06-07 12:38:00 +0530

    updated object.h

---
+++ object.h
@@ -0,0 +1,22 @@
+#ifndef OBJECT_H
+#define OBJECT_H
+
+#define MAX_OBJECTS 100
+
+typedef struct
+{
+    int id;
+    int type;
+    int active;
+    int params[10];
+} Object;
+
+extern Object objects[MAX_OBJECTS];
+extern int objectCount;
+
+void addObject();
+void deleteObject();
+void modifyObject();
+void redrawCanvas();
+
+#endif
